f8c3f6db53d400501d2024d92e2228a8ff151f58
Alexandre Amory <alexandre.amory@santannapisa.it>
2021-10-05 17:51:14 +0200

    including dataflow pragma for mat_mult

--- ips/mat_mult/hw/src/mat_mult.cpp
+++ ips/mat_mult/hw/src/mat_mult.cpp
@@ -14,6 +14,8 @@
 
 #include <cstring>
 #include <stdint.h>
+// TODO: in the future, try something similar to this Xilinx builtin matrix multiplication, which deprecated in 2019.2 
+//#include "hls_linear_algebra.h"
 #include "mat_mult_top.hpp"
 #include "mat_mult.hpp"
 
@@ -29,6 +31,15 @@ void mat_mult(volatile args_t *id, volatile data_t *mem_port_in, volatile data_t
 	data_t mat_p[MAT_SIZE][MAT_SIZE];
 	int_data_t result, mult;
 
+	// TODO investigate the use of dataflow for bigger matrix
+	// The dataflow pragma seems to work to reduce the latency only when the matrix size is smaller.
+	// For example, the design with size 32 didnt work with dataflow, but it works without it.
+	// The same happens with MAT_SIZE == 16. Although there was a latency reduction from 1820 cycles wo dataflow to 1060 w dataflow, the design does not work i nthe FPGA
+#if MAT_SIZE == 8
+	// MAT_SIZE == 8 has latency of 479 cycles wo dataflow and 290 w dataflow
+	// dataflow reduced II of the 1st loop from 2 to 1, while the 2nd and main loop from 4 to 1.
+ 	#pragma HLS DATAFLOW
+#endif
 	// factor of 4 cause these warnings below which means that the latency is suboptimal due to the lack of enough memory ports to read/write form the memories
 	// WARNING: [SCHED 204-69] Unable to satisfy resource constraint for operation type mul(II = 1)..
 	// WARNING: [SCHED 204-69] Unable to schedule 'load' operation ('mat_a_0_load_4', src/mat_mult.cpp:73->src/mat_mult_top.cpp:31->src/mat_mult_top.cpp:16) on array 'mat_a[0]', src/mat_mult.cpp:26->src/mat_mult_top.cpp:31->src/mat_mult_top.cpp:16 due to limited memory ports. Please consider using a memory core with more ports or partitioning the array 'mat_a_0'.
@@ -87,6 +98,9 @@ void mat_mult(volatile args_t *id, volatile data_t *mem_port_in, volatile data_t
 			mat_p[i][j] = (data_t)result;
 		}
 	}
+
+	// uncomment this to use xilinx builtin matrix multiplication
+	// hls::matrix_multiply <hls::NoTranspose, hls::NoTranspose, MAT_SIZE, MAT_SIZE, MAT_SIZE, MAT_SIZE, MAT_SIZE, MAT_SIZE, int_data_t, data_t> (mat_a, mat_b, mat_p);
 
 	// Copy local memory contents to output
 	std::memcpy((void *)( mem_port_out + args2 / sizeof(data_t) ),
