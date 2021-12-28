0fcdb568a24900710836be79e6701b29bb82d628
Alexandre Amory <amamory@gmail.com>
2021-12-28 11:53:57 +0100

    fixing the script for vitis_hls v2020.2

--- scripts/ip_hls.tcl
+++ scripts/ip_hls.tcl
@@ -3,7 +3,12 @@ set i 0
 while {$i < $num_slots} {
   set_top ${top_name}
   open_solution -reset "solution_$i"
-  set_part ${device} -tool vivado
+  set_part ${device}
+  # 64-bit AXI-M address capability
+  # This is supported by FRED, but currently not by DART.
+  # Therefore, we stick with 32 bits C_M_AXI_MEM_BUS_ADDR_WIDTH even for ZynqM devices
+  # The second implication is that FRED server MUST BE compiled without the define `HW_TASKS_A64`, to make it compatible with 32 bits
+  #config_interface -m_axi_addr64  
   create_clock -period ${period} -name default
 
   if {$sim != 0} {
